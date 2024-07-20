339455b7a1d541f191bffd15d5128cf1bbfeab5e
Bego Blanco <begoblanfer@gmail.com>
2024-07-20 21:39:20 +0200

    added the cancel function in promt

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ import { DNI } from "./src/js/dni.js";
 const prompt = promptSync();
 
 function askForDNI() {
-  const input = prompt("Enter your DNI number: ");
+  const input = prompt("Enter your DNI number (or type 'cancel' to exit): ");
 
-  const dniNumber = parseInt(input, 10);
-
-  if (isNaN(dniNumber) || dniNumber < 0 || dniNumber > 99999999) {
-    console.log("The input is not a valid number.");
+  if (input.toLowerCase() === "cancel") {
+    console.log("You canceled.");
   } else {
-    const dni = new DNI(dniNumber);
-    const letter = dni.letterCalculator();
-    console.log(`The letter for the DNI ${dniNumber} is ${letter}`);
+    const dniNumber = parseInt(input, 10);
+
+    if (isNaN(dniNumber) || dniNumber < 0 || dniNumber > 99999999) {
+      console.log("The input is not a valid number.");
+      askForDNI();
+    } else {
+      const dni = new DNI(dniNumber);
+      const letter = dni.letterCalculator();
+      console.log(`The letter for the DNI ${dniNumber} is ${letter}`);
+    }
   }
 }
 askForDNI();
